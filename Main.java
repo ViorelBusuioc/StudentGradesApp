94ca04e6e4d22cfad0581457f98d79ef5bc7861f
ViorelBusuioc <129962725+ViorelBusuioc@users.noreply.github.com>
2023-08-03 16:58:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,30 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Integer five = 5;
-        Integer[] others = {0,5,10,-50,50};
-
-        for (Integer i : others) {
-            int val = five.compareTo(i);
-            System.out.printf("%d %s %d: compareTo = %d%n", five,
-                    (val == 0 ? "==" : (val < 0) ? "<" : ">"), i, val);
-        }
-
-        String banana = "banana";
-        String[] fruit = {"apple","banana","pear","BANANA"};
-        for (String s : fruit) {
-            int val = banana.compareTo(s);
-            System.out.printf("%s %s %s: compareTo = %d%n", banana,
-                    (val == 0 ? "==" : (val < 0) ? "<" : ">"), s, val);
-        }
-
-        Arrays.sort(fruit);
-        System.out.println(Arrays.toString(fruit));
-
-        System.out.println("A:"+(int)+'A'+" "+"a:"+(int)+'a');
-        System.out.println("B:"+(int)+'B'+" "+"b:"+(int)+'b');
-        System.out.println("P:"+(int)+'P'+" "+"p:"+(int)+'p');
-
         Student tim = new Student("Tim");
         Student [] students = {new Student("Zack"),new Student("Tim"),new Student("Ann")};
         Arrays.sort(students);
@@ -75,10 +51,4 @@ class Student implements Comparable<Student> {
     public int compareTo(Student o) {
         return Integer.valueOf(id).compareTo(Integer.valueOf(o.id));
     }
-
-//    @Override
-//    public int compareTo(Object o) {
-//        Student other = (Student) o;
-//        return name.compareTo(other.name);
-//    }
 }
